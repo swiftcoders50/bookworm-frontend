a875012652191b617bc631ee15d7459a71c7053c
samihaTasnim <samxtasnim@gmail.com>
2021-09-08 15:26:17 +0600

    resolved conflicts

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
 import Head from 'next/head';
-<<<<<<< HEAD
 import Authors from '../components/Authors/Authors';
 import BestSeller from '../components/BestSeller/BestSeller';
 import UnreleasedBooks from '../components/UnreleasedBooks/UnreleasedBooks';
-=======
 import TopRatedCarousel from '../components/TopRatedCarousel';
->>>>>>> samiha-carousel-comp
 
 export default function Home() {
 	return (
 		<div>
-<<<<<<< HEAD
 			{/* best seller book component */}
 			<BestSeller />
 			{/* Unreleased books carousel component */}
 			<UnreleasedBooks />
 			{/* Authors carousel component*/}
 			<Authors />
-=======
 			<Head>
 				<title>Create Next App</title>
 				<link rel='icon' href='/favicon.ico' />
@@ -26,7 +21,6 @@ export default function Home() {
 			{/* samiha */}
 			<TopRatedCarousel></TopRatedCarousel>
 			{/* samiha */}
->>>>>>> samiha-carousel-comp
 		</div>
 	);
 }
