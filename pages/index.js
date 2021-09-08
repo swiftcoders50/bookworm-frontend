6848c7712a0c7dfb44f8d4759ebbde08bd6faa09
Masrur Sakib <mashrur333@gmail.com>
2021-09-08 15:43:04 +0600

    All single quotes replaced with double quotes

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,28 @@
-import Head from 'next/head';
-import Authors from '../components/Authors/Authors';
-import BestSeller from '../components/BestSeller/BestSeller';
-import UnreleasedBooks from '../components/UnreleasedBooks/UnreleasedBooks';
-import TopRatedCarousel from '../components/TopRatedCarousel';
+import Head from "next/head";
+import Authors from "../components/Authors/Authors";
+import BestSeller from "../components/BestSeller/BestSeller";
+import UnreleasedBooks from "../components/UnreleasedBooks/UnreleasedBooks";
+import TopRatedCarousel from "../components/TopRatedCarousel";
 
 export default function Home() {
-	return (
-		<div>
-			{/* best seller book component */}
-			<BestSeller />
-			{/* Unreleased books carousel component */}
-			<UnreleasedBooks />
-			{/* Authors carousel component*/}
-			<Authors />
-			<Head>
-				<title>Create Next App</title>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-			{/* samiha */}
-			<TopRatedCarousel></TopRatedCarousel>
-			{/* samiha */}
-		</div>
-	);
+      {/* samiha */}
+      <TopRatedCarousel></TopRatedCarousel>
+
+      {/* apel */}
+      <BestSeller />
+
+      {/* apel */}
+      <UnreleasedBooks />
+
+      {/* apel */}
+      <Authors />
+    </div>
+  );
 }
