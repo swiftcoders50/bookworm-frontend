5bfae30ea3755a6e73d15bcde62f826c9e281186
md jahidul islam <za0162hid@gmail.com>
2021-09-14 18:07:21 +0600

    foreignbook cart added

--- components/ForeignBooks/ForeignBooks.js
+++ components/ForeignBooks/ForeignBooks.js
@@ -1,9 +1,10 @@
-import React from 'react';
 import Books from "../Books/Books";
 import Link from "next/link";
+import { CartContext } from "../../pages/_app";
+import React, { useContext } from "react";
 
 const ForeignBooks = () => {
-      // demo array of books
+  // demo array of books
   const fBooks = [
     {
       id: 1,
@@ -49,38 +50,43 @@ const ForeignBooks = () => {
       img: "/assets/book-images/105.jpg",
     },
   ];
-    return (
-         // Foreign container
+
+  const [cartData, setCardData] = useContext(CartContext);
+  const handleBuy = (singleBook) => {
+    setCardData([...cartData, singleBook]);
+  };
+  return (
+    // Foreign container
     <div className="mx-2 sm:mx-4 md:mx-16">
-    <div className="flex justify-between my-8 border-b border-gray-200">
-      <h1 className="text-2xl md:text-3xl font-semibold text-indigo-900">
-        Foreign Books
-      </h1>
-      <Link href="/">
-        <a className="text-indigo-900 hover:text-indigo-500 text-md border border-b-0 p-2 font-semibold">
-          View All
-        </a>
-      </Link>
-    </div>
+      <div className="flex justify-between my-8 border-b border-gray-200">
+        <h1 className="text-2xl md:text-3xl font-semibold text-indigo-900">
+          Foreign Books
+        </h1>
+        <Link href="/">
+          <a className="text-indigo-900 hover:text-indigo-500 text-md border border-b-0 p-2 font-semibold">
+            View All
+          </a>
+        </Link>
+      </div>
 
-    {/* Foreign Cart container */}
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8 my-4 cursor-pointer">
-      {/* cart */}
-      {fBooks.map((book) => (
-        <Books key={book.id} book={book} />
-      ))}
-    </div>
+      {/* Foreign Cart container */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8 my-4 cursor-pointer">
+        {/* cart */}
+        {fBooks.map((book) => (
+          <Books key={book.id} book={book} handleBuy={handleBuy} />
+        ))}
+      </div>
 
-    {/* load more button */}
-    <div className="mx-auto text-center  bg-gray-100 rounded-md w-full md:w-1/2 p-2 my-8">
-      <Link href="/">
-        <a className="text-indigo-900 hover:text-indigo-500 uppercase text-lg font-semibold ">
-          More Foreign books
-        </a>
-      </Link>
+      {/* load more button */}
+      <div className="mx-auto text-center  bg-gray-100 rounded-md w-full md:w-1/2 p-2 my-8">
+        <Link href="/">
+          <a className="text-indigo-900 hover:text-indigo-500 uppercase text-lg font-semibold ">
+            More Foreign books
+          </a>
+        </Link>
+      </div>
     </div>
-  </div>
-    );
+  );
 };
 
 export default ForeignBooks;
