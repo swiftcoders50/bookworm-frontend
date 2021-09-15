3c55747edef18d4ae04f350f9c641d31f60f586d
md jahidul islam <za0162hid@gmail.com>
2021-09-15 15:10:03 +0600

    blog state fixing

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "../firebase.config";
 const app = initializeApp(firebaseConfig);
 import {
-	getAuth,
-	signOut,
-	updateProfile,
-	onAuthStateChanged,
-	sendPasswordResetEmail,
-	signInWithEmailAndPassword,
-	createUserWithEmailAndPassword,
+  getAuth,
+  signOut,
+  updateProfile,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 const auth = getAuth();
 
@@ -20,60 +20,60 @@ const AuthContext = createContext();
 
 // we will can call it from any components and get values
 export const useAuth = () => {
-	return useContext(AuthContext);
+  return useContext(AuthContext);
 };
 
 const AuthProvider = ({ children }) => {
-	const [loading, setLoading] = useState(false);
-	const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(false);
+  const [currentUser, setCurrentUser] = useState();
 
-	// sign up user method
-	const signup = async (name, email, password) => {
-		await createUserWithEmailAndPassword(auth, email, password);
-		await updateProfile(auth.currentUser, {
-			displayName: name,
-		});
-	};
+  // sign up user method
+  const signup = async (name, email, password) => {
+    await createUserWithEmailAndPassword(auth, email, password);
+    await updateProfile(auth.currentUser, {
+      displayName: name,
+    });
+  };
 
-	console.log(currentUser);
+  console.log(currentUser);
 
-	// login user method
-	const login = (email, password) => {
-		return signInWithEmailAndPassword(auth, email, password);
-	};
+  // login user method
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-	// logout user method
-	const logout = () => {
-		return signOut(auth);
-	};
+  // logout user method
+  const logout = () => {
+    return signOut(auth);
+  };
 
-	// sent password reset email
-	const passwordResetEmail = (email) => {
-		return sendPasswordResetEmail(auth, email);
-	};
+  // sent password reset email
+  const passwordResetEmail = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
 
-	// get user information after login
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, (user) => {
-			setCurrentUser(user);
-			setLoading(true);
-		});
-		return unsubscribe;
-	});
+  // get user information after login
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(true);
+    });
+    return unsubscribe;
+  });
 
-	const value = {
-		signup,
-		login,
-		logout,
-		currentUser,
-		passwordResetEmail,
-	};
+  const value = {
+    signup,
+    login,
+    logout,
+    currentUser,
+    passwordResetEmail,
+  };
 
-	return (
-		<AuthContext.Provider value={value}>
-			{loading && children}
-		</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider value={value}>
+      {loading && children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
