ca20951bcaf2ad06e91573fbeaf311b695ff1f65
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-09-16 12:04:33 +0600

    Navbar rearranged

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -3,72 +3,70 @@ import Link from "next/link";
 import { CartContext } from "../../pages/_app";
 
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false);
-  const [navbar, setNavbar] = useState(false);
-  const [cartData, setCardData] = useContext(CartContext);
+	const [showMenu, setShowMenu] = useState(false);
+	const [navbar, setNavbar] = useState(false);
+	const [cartData, setCardData] = useContext(CartContext);
 
-  const changeBackground = () => {
-    if (window.scrollY >= 70) {
-      setNavbar(true);
-    } else {
-      setNavbar(false);
-    }
-  };
-  useEffect(() => {
-    window.addEventListener("scroll", changeBackground);
-  }, []);
-  return (
-    <>
-      <nav
-        className={`fixed w-full z-10 flex items-center py-3 flex-wrap px-2 md:px-8 lg:px-16 ${
-          navbar ? "bg-blue-300" : "bg-gray-200"
-        }`}
-      >
-        <Link href="/">
-          <a className="p-2 mr-4 inline-flex items-center">
-            <span className="text-xl text-indigo-900 font-bold uppercase tracking-wide">
-              BOOKWORM
-            </span>
-          </a>
-        </Link>
+	const changeBackground = () => {
+		if (window.scrollY >= 70) {
+			setNavbar(true);
+		} else {
+			setNavbar(false);
+		}
+	};
+	useEffect(() => {
+		window.addEventListener("scroll", changeBackground);
+	}, []);
+	return (
+		<>
+			<nav
+				className={`fixed w-full z-10 flex items-center py-3 flex-wrap px-2 md:px-8 lg:px-16 ${
+					navbar ? "bg-blue-300" : "bg-gray-200"
+				}`}
+			>
+				<Link href="/">
+					<a className="p-2 mr-4 inline-flex items-center">
+						<img src="./logo.svg" alt="logo" />
+					</a>
+				</Link>
 
-        <button
-          onClick={() => setShowMenu(!showMenu)}
-          className="text-gray-600 font-bold inline-flex p-3 hover:bg-gray-900 rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler"
-        >
-          <svg
-            className="w-10"
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
-        <div
-          className={`${
-            showMenu === false ? "hidden" : ""
-          } top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto`}
-        >
-          <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
-            <Link href="/">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:bg-indigo-900 hover:text-white">
-                <span>Home</span>
-              </a>
-            </Link>
+				<button
+					onClick={() => setShowMenu(!showMenu)}
+					className="text-gray-600 font-bold inline-flex p-3 hover:bg-gray-900 rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler"
+				>
+					<svg
+						className="w-10"
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						/>
+					</svg>
+				</button>
+				<div
+					className={`${
+						showMenu === false ? "hidden" : ""
+					} top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+				>
+					<div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
+						<Link href="/">
+							<a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:bg-indigo-900 hover:text-white">
+								<span>Home</span>
+							</a>
+						</Link>
 
-            <Link href="/categories">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:hover:bg-indigo-900 hover:text-white">
-                <div className="flex">
-                  <span>Categories</span>
-                  {/* <span>
+						{/* <Link href="/categories">
+							<a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:hover:bg-indigo-900 hover:text-white">
+								<div className="flex">
+									<span>Categories</span>
+									<span>
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
 											class="h-6 w-5"
@@ -81,12 +79,12 @@ const Header = () => {
 												clip-rule="evenodd"
 											/>
 										</svg>
-									</span> */}
-                </div>
-              </a>
-            </Link>
-            {/* this is dropdown */}
-            {/* <div class="group inline-block relative">
+									</span> 
+								</div>
+							</a>
+						</Link>*/}
+						{/* this is dropdown */}
+						{/* <div class="group inline-block relative">
 							<div class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-600 font-bold items-center justify-center hover:hover:bg-indigo-900 hover:text-white">
 								<span class="mr-1">Categories</span>
 								<svg
@@ -124,63 +122,63 @@ const Header = () => {
 								</li>
 							</ul>
 						</div> */}
-            {/* end of dropdown */}
-            <Link href="/bestSeller">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:hover:bg-indigo-900 hover:text-white">
-                <span>Best Sellers</span>
-              </a>
-            </Link>
+						{/* end of dropdown */}
+						{/* <Link href="/bestSeller">
+							<a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:hover:bg-indigo-900 hover:text-white">
+								<span>Best Sellers</span>
+							</a>
+						</Link> */}
 
-            <Link href="/contact">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:hover:bg-indigo-900 hover:text-white">
-                <span>Contact</span>
-              </a>
-            </Link>
+						<Link href="/contact">
+							<a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:hover:bg-indigo-900 hover:text-white">
+								<span>Contact</span>
+							</a>
+						</Link>
 
-            <Link href="/dashboard">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:hover:bg-indigo-900 hover:text-white">
-                <span>Dashboard</span>
-              </a>
-            </Link>
+						<Link href="/dashboard">
+							<a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:hover:bg-indigo-900 hover:text-white">
+								<span>Dashboard</span>
+							</a>
+						</Link>
 
-            <Link href="/login">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:hover:bg-indigo-900 hover:text-white">
-                <span>Login</span>
-              </a>
-            </Link>
-            {/* cart icon*/}
-            <Link href="/order_review">
-              <div className="flex ml-3 sm:mt-0 mt-3 text-indigo-900">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
-                <div
-                  className="text-gray-500"
-                  style={{
-                    marginTop: "-10px",
-                    fontWeight: "700",
-                  }}
-                >
-                  {cartData.length}
-                </div>
-              </div>
-            </Link>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+						<Link href="/login">
+							<a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-indigo-900 font-semibold items-center justify-center hover:hover:bg-indigo-900 hover:text-white">
+								<span>Login</span>
+							</a>
+						</Link>
+						{/* cart icon*/}
+						<Link href="/order_review">
+							<div className="flex ml-3 sm:mt-0 mt-3 text-indigo-900 cursor-pointer">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-5 w-5"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+									/>
+								</svg>
+								<div
+									className="text-gray-500"
+									style={{
+										marginTop: "-10px",
+										fontWeight: "700",
+									}}
+								>
+									{cartData.length}
+								</div>
+							</div>
+						</Link>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
 };
 
 export default Header;
