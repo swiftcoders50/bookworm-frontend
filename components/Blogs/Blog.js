db65633dc8f73476167a6a98e53ccf94b83220e3
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-09-18 10:45:14 +0600

    Blog section responsive done

--- components/Blogs/Blog.js
+++ components/Blogs/Blog.js
@@ -1,37 +1,34 @@
 import { FaRegCalendarAlt } from "react-icons/fa";
 import Link from "next/link";
 const Blog = ({ blog }) => {
-  return (
-    <div>
-      <img
-        className="h-2/4 w-full object-cover mb-3"
-        src={blog.img}
-        alt={blog.name}
-      />
-      <h4>
-        <b>By {blog.name}</b>
-      </h4>
+	return (
+		<div>
+			<img
+				className="h-2/4 w-full object-cover mb-3"
+				src={blog.img}
+				alt={blog.name}
+			/>
+			<h4 className="text-gray-800">
+				<b>By {blog.name}</b>
+			</h4>
 
-      <div className="flex gap-2">
-        <FaRegCalendarAlt
-          className="mt-1 cursor-pointer"
-          size="1rem"
-          color="gray"
-        />
-        <h4>{blog.date}</h4>
-      </div>
-      <h4 className="text-xl my-3">
-        <b>{blog.title}</b>
-      </h4>
-      <p>{blog.text}</p>
-
-      <Link href={"/blogDetails/" + blog.id}>
-        <button className="bg-indigo-900 hover:bg-indigo-800 inline-block text-white rounded-full text-md px-5 py-1 my-3 font-semibold">
-          Read More
-        </button>
-      </Link>
-    </div>
-  );
+			<div className="flex items-center gap-2">
+				<FaRegCalendarAlt className="cursor-pointer" size="1rem" color="gray" />
+				<h4 className="text-gray-500">{blog.date}</h4>
+			</div>
+			<h4 className="text-xl text-gray-800 my-3">
+				<b>{blog.title}</b>
+			</h4>
+			<p className="text-gray-800">{blog.text}</p>
+			<Link href={"/blogDetails/" + blog.id}>
+				<a>
+					<button className="bg-indigo-900 active:bg-indigo-800 inline-block text-white rounded-full text-md px-5 py-1 my-4 font-medium focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50">
+						Read More
+					</button>
+				</a>
+			</Link>
+		</div>
+	);
 };
 
 export default Blog;
--- components/Blogs/Blogs.js
+++ components/Blogs/Blogs.js
@@ -2,58 +2,60 @@ import { useContext, useEffect } from "react";
 import { BlogContext } from "../../pages/_app";
 import Blog from "./Blog";
 const Blogs = () => {
-  const blogs = [
-    {
-      id: 1,
-      name: "Ramamoorthi M",
-      date: "July 5, 2021",
-      title: "Books changed my ideology",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis suscipit ligula id erat accumsan eleifend ut ac metus. Aliquam nec arcu et felis tincidunt vestibulum et eget ante. ",
-      img: "/assets/blog-images/blog-1.jpg",
-    },
-    {
-      id: 2,
-      name: "Ramamoorthi M",
-      date: "July 5, 2021",
-      title: "Best writers of 19th century",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis suscipit ligula id erat accumsan eleifend ut ac metus. Aliquam nec arcu et felis tincidunt vestibulum et eget ante. ",
-      img: "/assets/blog-images/blog-2.jpg",
-    },
-    {
-      id: 3,
-      name: "Ramamoorthi M",
-      date: "July 5, 2021",
-      title: "My favourite books of 2021",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis suscipit ligula id erat accumsan eleifend ut ac metus. Aliquam nec arcu et felis tincidunt vestibulum et eget ante. ",
-      img: "/assets/blog-images/blog-3.jpg",
-    },
-  ];
-  //context api for the blog
-  const [blogData, setBlogData] = useContext(BlogContext);
+	const blogs = [
+		{
+			id: 1,
+			name: "Ramamoorthi M",
+			date: "July 5, 2021",
+			title: "Books changed my ideology",
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis suscipit ligula id erat accumsan eleifend ut ac metus. Aliquam nec arcu et felis tincidunt vestibulum et eget ante. ",
+			img: "/assets/blog-images/blog-1.jpg",
+		},
+		{
+			id: 2,
+			name: "Ramamoorthi M",
+			date: "July 5, 2021",
+			title: "Best writers of 19th century",
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis suscipit ligula id erat accumsan eleifend ut ac metus. Aliquam nec arcu et felis tincidunt vestibulum et eget ante. ",
+			img: "/assets/blog-images/blog-2.jpg",
+		},
+		{
+			id: 3,
+			name: "Ramamoorthi M",
+			date: "July 5, 2021",
+			title: "My favourite books of 2021",
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis suscipit ligula id erat accumsan eleifend ut ac metus. Aliquam nec arcu et felis tincidunt vestibulum et eget ante. ",
+			img: "/assets/blog-images/blog-3.jpg",
+		},
+	];
+	//context api for the blog
+	const [blogData, setBlogData] = useContext(BlogContext);
 
-  useEffect(() => {
-    setBlogData(blogs);
-  }, []);
+	useEffect(() => {
+		setBlogData(blogs);
+	}, []);
 
-  // console.log(blogData);
+	// console.log(blogData);
 
-  return (
-    <section className="px:10 md:px-20 lg:px-40 pb-20">
-      <div className="flex justify-between">
-        <h2 className="text-2xl md:text-3xl font-semibold text-indigo-900">
-          Bookworm Blogs
-        </h2>
-        {/* <button className="bg-indigo-900 hover:bg-indigo-800 inline-block text-white rounded-full text-md px-5 py-1 font-semibold">
-          Read all Blogs
-        </button> */}
-      </div>
-      <div className="py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14">
-        {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog}></Blog>
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<section className="mx-3 sm:mx-4 md:mx-16">
+			<div className="flex justify-between my-4 border-b border-gray-200">
+				<h1 className="font-oswald text-xl md:text-2xl font-medium text-indigo-900">
+					Bookworm Blogs
+				</h1>
+				{/* <Link href="/">
+					<a className="font-oswald text-indigo-900 hover:text-indigo-500 text-md border border-b-0 p-2 font-medium">
+						View All
+					</a>
+				</Link> */}
+			</div>
+			<div className="py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14">
+				{blogs.map((blog) => (
+					<Blog key={blog.id} blog={blog}></Blog>
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default Blogs;
