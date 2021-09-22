6e5806c70b9fb8cfa0db3c85264f408a26adbc9e
md jahidul islam <za0162hid@gmail.com>
2021-09-22 18:04:08 +0600

    live chat added

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import ScrolledTop from "../components/ScrolledTop/ScrolledTop";
+import LiveChat from "../components/LiveChat/LiveChat";
 
 const Footer = () => {
   return (
