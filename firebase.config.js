926507f03ef92e3583d5aafe80790b270f606b9c
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-09-27 10:45:15 +0600

    remove sensitive data

--- firebase.config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDaN-Dj8oFd8GEUEekToPG0soQqdvT2O7g",
-  authDomain: "bookworm-17468.firebaseapp.com",
-  projectId: "bookworm-17468",
-  storageBucket: "bookworm-17468.appspot.com",
-  messagingSenderId: "222189275314",
-  appId: "1:222189275314:web:ff1907e9b64804046021ab",
-};
