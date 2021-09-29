ce6dd0f81265d0d45a032f6158c760450f255db6
swiftcoders50 <swiftcoders50@gmail.com>
2021-09-29 22:03:45 +0600

    Dashboard header text color changed.

--- components/DashboardHeader.js
+++ components/DashboardHeader.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { useAuth } from "../contexts/AuthContext";
+import { FiLogOut } from "react-icons/fi";
 
 const DashboardHeader = ({ sectionTitle }) => {
   const { currentUser, logout } = useAuth();
@@ -10,17 +11,17 @@ const DashboardHeader = ({ sectionTitle }) => {
         <h2 className="text-gray-700 text-lg">{sectionTitle}</h2>
       </div>
       <div className="flex justify-end items-center ">
-        <p
-          className="mx-2 text-gray-700 text-lg cursor-pointer "
-          onClick={logout}
-        >
-          Logout
-        </p>
         <Link href="/my-profile">
-          <a className="mx-2 text-gray-700 text-lg cursor-pointer">
+          <a className="mx-2 text-indigo-800 text-lg font-semibold cursor-pointer">
             {currentUser.displayName}
           </a>
         </Link>
+        <div className="text-red-600 flex justify-center items-center">
+          <p className="mx-2 text-lg cursor-pointer " onClick={logout}>
+            Logout
+          </p>
+          <FiLogOut />
+        </div>
       </div>
     </div>
   );
