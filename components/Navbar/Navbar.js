2d498020eb58b300feec87ec6a014eb4c3a58772
unknown <za0162hid@gmail.com>
2021-10-06 23:06:33 +0600

    navbar edit

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -138,9 +138,9 @@ const Header = () => {
                 <span>Dashboard</span>
               </a>
             </Link>
-            {/* cart icon*/}
 
-            <Link href="/order_review">
+            {/* cart icon*/}
+            {/* <Link href="/order_review">
               <a>
                 <div className="flex ml-3 my-3 lg:my-0 text-indigo-900 cursor-pointer">
                   <svg
@@ -168,7 +168,8 @@ const Header = () => {
                   </div>
                 </div>
               </a>
-            </Link>
+            </Link> */}
+            {/* cart icon end*/}
 
             {/* login button menu */}
             {!currentUser && (
@@ -245,6 +246,36 @@ const Header = () => {
             )}
           </div>
         </div>
+        {/* cart icon*/}
+        <Link href="/order_review">
+          <a>
+            <div className="flex ml-3 my-3 lg:my-0 text-indigo-900 cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <div
+                className="text-gray-500"
+                style={{
+                  marginTop: "-10px",
+                  fontWeight: "700",
+                }}
+              >
+                {cartData.length}
+              </div>
+            </div>
+          </a>
+        </Link>
       </nav>
     </>
   );
