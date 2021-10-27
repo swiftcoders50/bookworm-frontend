b707e95f3dae869a56c3f48fe54fba6fd0f298f2
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-10-27 21:07:56 +0600

    dashboard protected just available for admin

--- hook/dashboardRoute.js
+++ hook/dashboardRoute.js
@@ -2,12 +2,25 @@ import { useRouter } from "next/router";
 import { useAdmin } from "../contexts/AdminContext";
 import { useAuth } from "../contexts/AuthContext";
 
+export function withPublic(Component) {
+	return function WithPublic(props) {
+		const { admin } = useAdmin();
+		const router = useRouter();
+
+		if (admin) {
+			router.replace("/");
+			return <h1 className="text-lg">Loading...</h1>;
+		}
+		return <Component admin={admin.isAdmin} {...props} />;
+	};
+}
+
 export function dashboardWithProtected(Component) {
 	return function WithProtected(props) {
 		const { admin } = useAdmin();
 		const router = useRouter();
 
-		if (!admin.isAdmin) {
+		if (!admin) {
 			router.replace("/");
 			return <h1 className="text-lg h-32">Loading...</h1>;
 		}
